5f2e508a0758539ffed434d764f480de3996ac3c
nabeelraza-7 <razanabeel512@gmail.com>
2020-12-13 20:52:42 +0500

    New Editing Features added

--- src/notepad/MainController.java
+++ src/notepad/MainController.java
@@ -5,14 +5,51 @@ import java.io.*;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
 public class MainController{
+    private String currentFont = "Consolas";
+    private int currentSize = 14;
     public File selected;
     @FXML
     private TextArea textArea;
+
+    @FXML 
+    private MenuItem s12;
+    @FXML
+    private MenuItem s14;
+    @FXML
+    private MenuItem s16;
+    @FXML
+    private MenuItem s18;
+    @FXML
+    private MenuItem s20;
+    @FXML
+    private MenuItem s22;
+    @FXML
+    private MenuItem s24;
+    @FXML
+    private MenuItem s26;
+    @FXML
+    private MenuItem s28;
+    @FXML
+    private MenuItem s30;
+
+    @FXML 
+    private MenuItem arial;
+    @FXML
+    private MenuItem century;
+    @FXML
+    private MenuItem consolas;
+    @FXML
+    private MenuItem monospace;
+    @FXML
+    private MenuItem georgia;
+
     public void closeApp(ActionEvent e){
         Platform.exit();
         System.exit(0);
@@ -57,5 +94,60 @@ public class MainController{
         bw.write(textArea.getText());
         bw.close();
         System.out.println("File Saved");
+    }
+
+    public void changeFont(ActionEvent e){
+        if (((MenuItem) e.getSource()).getId() == arial.getId()){
+            currentFont = "Arial";
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == century.getId()){
+            currentFont = "Century";
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == consolas.getId()){
+            currentFont = "Consolas";
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == monospace.getId()){
+            currentFont = "Monospace";
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == georgia.getId()){
+            currentFont = "Georgia";
+            triggerChange();
+        }
+    }
+    public void changeSize(ActionEvent e){
+        if (((MenuItem) e.getSource()).getId() == s12.getId()){
+            currentSize = 12;
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == s14.getId()){
+            currentSize = 14;
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == s16.getId()){
+            currentSize = 16;
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == s18.getId()){
+            currentSize = 18;
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == s20.getId()){
+            currentSize = 20;
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == s22.getId()){
+            currentSize = 22;
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == s24.getId()){
+            currentSize = 24;
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == s26.getId()){
+            currentSize = 26;
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == s28.getId()){
+            currentSize = 28;
+            triggerChange();
+        } else if (((MenuItem) e.getSource()).getId() == s30.getId()){
+            currentSize = 30;
+            triggerChange();
+        }
+    }
+    public void triggerChange(){
+        textArea.setFont(Font.font(currentFont, currentSize));
     }
 }
